efac79826ab8b439cd2ef89a9be6c18b64abe295
wutengfei <wutengfei@bjca.org.cn>
2020-01-17 10:35:15 +0800

    master分支新增代码

--- app/src/main/java/cn/org/bjca/githubtest/MainActivity.java
+++ app/src/main/java/cn/org/bjca/githubtest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("这是dev分支");
+        System.out.println("这是master分支");
     }
 }
